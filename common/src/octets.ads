e5dd8c917f4d38f328148c1d23ff09f4db6bc394
John Serock <1468970+serock@users.noreply.github.com>
2022-02-09 18:57:14 -0500

    Update prologue

--- common/src/octets.ads
+++ common/src/octets.ads
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------
 --  pla-util - A powerline adapter utility
---  Copyright (C) 2021-2022 John Serock
+--  Copyright (C) 2016-2022 John Serock
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
